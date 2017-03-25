7fd84aa3177d84d35afca9a2281541f531a7f702
Nathan Blank <nathan@OVERMIND.rose-hulman.edu>
2017-03-24 20:39:47 -0400

    wow

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	profile: Ember.inject.service(),
 
+	email:'',
+	password:'',
+	actions:{
+		login(){
+			this.get('profile' ).login(this.get('email'), this.get('password'));
 
-
+		}
+		
+	},
 });
